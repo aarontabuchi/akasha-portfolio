55ab5fa9a157570fd765d0763efa0e97a6bf5e07
Aaron Tabuchi <aaronbuchi@gmail.com>
2020-12-02 14:26:32 -0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
+  siteMetadata: {
+    title: "Akasha Chamberlain",
+    titleTemplate: "%s · Akasha",
+    description:
+      "See content strategist and program manager, Akasha Chamberlain's, resume and portfolio.",
+    url: "https://www.akashachamberlain.com",
+    image: "/akasha.png"
+  },
   plugins: []
 }

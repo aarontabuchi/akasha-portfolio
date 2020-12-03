3eda44bd3d35ebbcc0f2e0f3bf74aab806699d23
Aaron Tabuchi <aaronbuchi@gmail.com>
2020-12-03 15:18:32 -0800

    add gsap

--- src/components/header.js
+++ src/components/header.js
@@ -1,48 +1,61 @@
 import React from "react"
 import Styles from "../styles/header.module.css"
 import NavBar from "./navBar"
+import { gsap } from "gsap"
 
 class Header extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {title: this.props.headerText[0], n: 0};
+    super(props)
+    this.state = { title: this.props.headerText[0], n: 0 }
+    this.imgContainer = null
+    this.imgTween = null
   }
-  
+
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      100
-    );
+    this.timerID = setInterval(() => this.tick(), 100)
+    this.props.animation
+      ? (this.imgTween = gsap.from(this.imgContainer, this.props.animation))
+      : (this.imgTween = null)
   }
 
   tick() {
     this.setState({
-      title: this.props.headerText.substring(0, this.state.n +1),
-      n : this.state.n + 1
+      title: this.props.headerText.substring(0, this.state.n + 1),
+      n: this.state.n + 1,
     })
   }
 
   componentWillUnmount() {
-    clearInterval(this.timerID);
+    clearInterval(this.timerID)
   }
 
   render() {
     return (
-    <header className={Styles.header}>
-      <div className={Styles.navigation + " wrapper"}>
-        <img src="../../akasha.png" alt="Akasha" height="200px" width="200px"></img>
-        <NavBar />
-      </div>
-      <div className={Styles.text + " wrapper"}>
-        <h1>{this.state.title}<span id="blinker" aria-hidden="true"></span></h1>
-        <div>
-          {this.props.paragraph.split("/n").map((item, i) => {
-            return <p key={i}>{item}</p>
-          })}
+      <header className={Styles.header}>
+        <div className={Styles.navigation + " wrapper"}>
+          <img
+            src="../../akasha.png"
+            alt="Akasha"
+            height="200px"
+            width="200px"
+            ref={img => (this.imgContainer = img)}
+          />
+          <NavBar />
         </div>
-      </div>
-    </header>)
+        <div className={Styles.text + " wrapper"}>
+          <h1>
+            {this.state.title}
+            <span id="blinker" aria-hidden="true"></span>
+          </h1>
+          <div>
+            {this.props.paragraph.split("/n").map((item, i) => {
+              return <p key={i}>{item}</p>
+            })}
+          </div>
+        </div>
+      </header>
+    )
   }
 }
 
-export default Header;
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Header from "../components/header";
 import Styles from "../styles/main.module.css"
 import Footer from "../components/footer";
 
+const animate = {duration:3, opacity:0}
+
 export default function Home() {
   return (
     <div >
-      <Header headerText="Hello, I'm Akasha Chamberlain" 
+      <Header
+      animation={animate}
+      headerText="Hello, I'm Akasha Chamberlain" 
       paragraph="I'm a content strategist for Waymo (formerly the Google self-driving car project) /n
       I'm passionate about writing simple content that makes life easier for the people reading it"/>
       <div className={Styles.mainColumn + " wrapper"} >
